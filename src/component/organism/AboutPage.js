2de45d7b0888fa1f80203f197e10d4adefc2ead5
Phillip Chen <phillipchen@phillips-mbp-2.wireless-1x.unc.edu>
2022-10-25 20:46:03 -0400

    setup gh-pages 22th trial

--- src/component/organism/AboutPage.js
+++ src/component/organism/AboutPage.js
@@ -45,10 +45,10 @@ class AboutPage extends React.Component{
                     </div>
                     <div className='flex flex-col lg:flex-row justify-between mt-4 space-y-4 lg:space-y-0'>
                         <span className='border border-light-primary dark:border-dark-primary rounded-md w-full lg:w-30% text-center py-4 flex items-center justify-center'>
-                            <SubHeaderText color='default' className='font-bold' text={['Data Visualization']} />
+                            <SubHeaderText color='default' className='font-bold' text={['UI/UX Design']} />
                         </span>
                         <span className='border border-light-primary dark:border-dark-primary rounded-md w-full lg:w-30% text-center py-4 flex items-center justify-center'>
-                            <SubHeaderText color='default' className='font-bold' text={['UI/UX Design']} />
+                            <SubHeaderText color='default' className='font-bold' text={['Data Visualization']} />
                         </span>
                         <span className='border border-light-primary dark:border-dark-primary rounded-md w-full lg:w-30% text-center py-4'>
                             <SubHeaderText color='default' className='font-bold' text={['Front-end Development']} />
