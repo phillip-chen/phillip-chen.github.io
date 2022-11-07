c2f6abcde23b7b21f20f1f93a048ad292036297e
Phillip Chen <phillipchen@phillips-mbp-2.wireless-1x.unc.edu>
2022-11-06 19:33:30 -0500

    setup gh-pages 36th trial

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+
+import './style/main-style.css';
+import './style/logo-animation.css';
+// Fixed Components
+import Navigation from './component/molecular/Navigation';
+import Footer from './component/molecular/Footer';
+
+// Routing Pages
+import ToTopBtn from './component/atom/ToTopBtn';
+import HomePage from './component/organism/HomePage';
+import AboutPage from './component/organism/AboutPage';
+// Case Study Page
+import ChatBot from './component/organism/uiux-project/ChatbotProject';
+import Toshiba from './component/organism/uiux-project/Toshiba';
+import ARProject from './component/organism/uiux-project/ARProject';
+import InfoDesign from './component/organism/uiux-project/InfoDesign';
+import ReactGa from 'react-ga';
+
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        
+        ReactGa.initialize('G-GLHKSTM3M9');
+        // to report page view
+        ReactGa.pageview(window.location.pathname + window.location.search);
+    }
+
+    render(){
+        return(
+            <HashRouter>      
+                <Navigation />
+                <Routes>
+                <Route path='/' element={<HomePage />}></Route>
+                <Route path='/about' element={<AboutPage />}></Route>
+                <Route path='/uiux-project/chatbot' element={<ChatBot />}></Route>
+                <Route path='/uiux-project/toshiba' element={<Toshiba />}></Route>
+                <Route path='/uiux-project/ar-game' element={<ARProject />}></Route>
+                <Route path='/uiux-project/info-design' element={<InfoDesign />}></Route>
+                </Routes>
+                <ToTopBtn className='to-top-btn fixed bottom-14 w-full sm:bottom-0 xl:right-4.5% laptop-md:right-6% laptop-lg:right-10% text-sm duration-0' text = 'Back To Top' sectionName = 'top' />
+                <Footer />
+            </HashRouter>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,75 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-
-import './style/main-style.css';
-import './style/logo-animation.css';
-// Fixed Components
-import Navigation from './component/molecular/Navigation';
-import Footer from './component/molecular/Footer';
-
-// Routing Pages
-import ToTopBtn from './component/atom/ToTopBtn';
-import HomePage from './component/organism/HomePage';
-import AboutPage from './component/organism/AboutPage';
-// Case Study Page
-import ChatBot from './component/organism/uiux-project/ChatbotProject';
-import Toshiba from './component/organism/uiux-project/Toshiba';
-import ARProject from './component/organism/uiux-project/ARProject';
-import InfoDesign from './component/organism/uiux-project/InfoDesign';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HashRouter>      
-    <Navigation />
-    <Routes>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} 
-      path='/' element={<HomePage />}></Route>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} path='/about' element={<AboutPage />}></Route>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} path='/uiux-project/chatbot' element={<ChatBot />}></Route>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} path='/uiux-project/toshiba' element={<Toshiba />}></Route>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} path='/uiux-project/ar-game' element={<ARProject />}></Route>
-      <Route render={({location}) => {
-          if (typeof window.ga === 'function') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-          }
-          return null;
-        }} path='/uiux-project/info-design' element={<InfoDesign />}></Route>
-    </Routes>
-    <ToTopBtn className='to-top-btn fixed bottom-14 w-full sm:bottom-0 xl:right-4.5% laptop-md:right-6% laptop-lg:right-10% text-sm duration-0' text = 'Back To Top' sectionName = 'top' />
-    <Footer />
-  </HashRouter>
+  <App />
 );
