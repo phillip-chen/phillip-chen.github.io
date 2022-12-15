f676833b7636781b6107b956b91aeda1e478c0e4
Phillip Chen <phillipchen@Phillips-MacBook-Pro-2.local>
2022-12-15 12:14:21 +0800

    setup gh-pages 42nd trial

--- src/component/atom/ProjectTag.js
+++ src/component/atom/ProjectTag.js
@@ -10,7 +10,7 @@ class ProjectTag extends React.Component{
             <div className='flex space-x-1.5'>
                 {
                     this.props.tags.map((tagText, index)=>{
-                        return <span key={index} className={'tag border dark:text-dark-primary dark:border-dark-primary px-2 py-1 sm:px-3 sm:py-0.5 rounded-sm text-center text-xs sm:text-sm'+' '+this.props.className}>{tagText}</span>
+                        return <span key={index} className={'tag border dark:text-dark-primary dark:border-dark-primary px-2 py-1 sm:px-3 sm:py-0.5 rounded-sm text-center text-xs sm:text-sm flex items-center'+' '+this.props.className}>{tagText}</span>
                     })
                 }
             </div>
